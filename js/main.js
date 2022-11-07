227d450240b36ceded0d58d0962109e3cc55754f
차은엽 <eycha@cha-eun-yeob-ui-MacBookAir.local>
2022-11-07 23:54:10 +0900

    remake

--- js/main.js
+++ js/main.js
@@ -2,21 +2,27 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 
-searchEl.addEventListener('click', function() {
+searchEl.addEventListener('click', function () {
     //Logic..
     searchInputEl.focus();
 
 });
 
 
-searchInputEl.addEventListener('focus', function() {
+searchInputEl.addEventListener('focus', function () {
     searchEl.classList.add('focused');
     searchInputEl.setAttribute('placeholder', '통합검색');
 
 });
 
-searchInputEl.addEventListener('blur', function() {
+searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
+
+});
+
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener('scroll', function () {
+    console.log('scroll!');
 
 });
