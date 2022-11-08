3c2d80bc0430e073bb8d305408f6ef155915893e
eycha <silvlexonst@gmail.com>
2022-11-08 17:45:49 +0900

    until promotion

--- js/main.js
+++ js/main.js
@@ -22,7 +22,52 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 const badgeEl = document.querySelector('header .badges');
-window.addEventListener('scroll', function () {
-    console.log('scroll!');
+window.addEventListener('scroll', _.throttle(function() {
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        //배지 숨기기
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity:0,
+            display: 'none'
+        });
+    } else {
+        //배지보여주기
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display: 'block'
+        });
+    }
+}, 300));
+//_.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, //0.7, 1.4, 2.1, 2.7
+        opacity: 1,
+
+    });
+});
+
+
+// new swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+});
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, //슬라이드 사이 여백
+    centeredSlides: true, //1번 슬라이드 가운데 보기
+    loop: true,
+    autoplay: {
+        delay: 5000
+    }
 
 });
+
+
